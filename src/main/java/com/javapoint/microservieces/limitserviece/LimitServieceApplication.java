b2299f8e42d149725951e6368b48e61e50dbdade
varun2108 <varun.kamma16@gmail.com>
2021-05-28 21:28:23 +0530

    first push

---
+++ src/main/java/com/javapoint/microservieces/limitserviece/LimitServieceApplication.java
@@ -0,0 +1,13 @@
+package com.javapoint.microservieces.limitserviece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitServieceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitServieceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javapoint/microservieces/limitserviece/LimitServieceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javapoint.microservieces.limitserviece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServieceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
